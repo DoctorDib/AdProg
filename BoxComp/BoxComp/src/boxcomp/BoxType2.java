4c788937f94a5b814028949cc10e63efcd517203
Jake Barter <midg3tjake@live.co.uk>
2016-11-27 19:17:51 +0000

    Abstractinated

---
+++ BoxComp/BoxComp/src/boxcomp/BoxType2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boxcomp;
+
+/**
+ *
+ * @author midgetjake
+ */
+public class BoxType2 extends Box{
+    
+    public BoxType2(int q, double s){
+        super(q, s);
+    }
+    
+    public double getPrice(){
+        return 0;
+    }
+}
---
+++ BoxComp/BoxComp/src/boxcomp/BoxType5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boxcomp;
+
+/**
+ *
+ * @author midgetjake
+ */
+public class BoxType5 extends Box{
+    
+    public BoxType5(int q, double s){
+        super(q, s);
+    }
+    
+    public double getPrice(){
+        return 0;
+    }
+    
+}
