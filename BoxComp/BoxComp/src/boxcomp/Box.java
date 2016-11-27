9dc2b541da24d13ce587be382c4a4e4277aae2c7
Ben <benjaminge.franklin@gmail.com>
2016-11-27 17:38:36 +0000

    Box superclass

---
+++ BoxComp/BoxComp/src/boxcomp/Box.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boxcomp;
+
+/**
+ * This class is used to define from the user input
+ * which box (subclass) is to be used.
+ * @author Ben, James, Jake
+ */
+public class Box {
+    
+}
