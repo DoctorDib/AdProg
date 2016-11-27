9f809aab60406643d99ac2fe9137be64ee6a7d4a
Jake Barter <midg3tjake@live.co.uk>
2016-11-27 20:07:54 +0000

    validation

--- BoxComp/BoxComp/src/boxcomp/Box.java
+++ BoxComp/BoxComp/src/boxcomp/Box.java
@@ -23,4 +23,42 @@ abstract class Box {
     
     abstract double getPrice();
     //no methods since subclasses will
+    
+    public String validate(int grade, int colour, boolean bottom, boolean corner){
+        boolean fail = false;
+        String cause = "";
+        if(corner&&!bottom){
+            fail = true;
+            cause += "Reinforced Bottom is required for Reinforced Corners \n";
+        }
+        if(bottom&&grade==1){
+            fail = true;
+            cause += "Grade 2 or higher is required for Reinforced Bottom \n";
+        }
+        if(corner&&grade<3){
+            fail = true;
+            cause += "Grade 3 or higher is required for Reinforced Corners \n";
+        }
+        if (colour!=2&&bottom){
+            fail = true;
+            cause += "2 Colours are required for Reinforced Bottom \n";
+        }
+        if(colour==2&&grade==1){
+            fail = true;
+            cause += "Grade 2 or higher is required for 2 Colours \n";
+        }
+        if((grade<2 || grade>4) && colour==2){
+            fail = true;
+            cause += "Grade 2-4 is required for 1 Colour \n";
+        }
+        if(colour==0&&grade>3){
+            fail = true;
+            cause += "Grade 3 or lower is required for no Colours \n";
+        }
+        if(fail){
+            return cause;
+        } else {
+            return "good";
+        }
+    }
 }
