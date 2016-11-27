0d2c1c073cd8456131b69ac41f85bb601b0a341a
Ben <benjaminge.franklin@gmail.com>
2016-11-27 16:51:27 +0000

    Should be able to test it now

--- BoxComp/BoxComp/src/boxcomp/Main.java
+++ BoxComp/BoxComp/src/boxcomp/Main.java
@@ -11,13 +11,14 @@ package boxcomp;
  *
  * @author Ben, James, Jake
  */
-public class BoxComp {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.prinln("hello world");
     }
     
 }
